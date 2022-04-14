6f349d2d7cf4918cec117a2e29e14d81d0d99595
shubhamlal059 <99653859+flayerrow@users.noreply.github.com>
2022-04-14 17:50:43 +0530

    Added

--- php/process.php
+++ php/process.php
@@ -1,8 +1,13 @@
 <?php 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Credentials: true ");
-    header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-    header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+    // header("Access-Control-Allow-Origin: *");
+    // header("Access-Control-Allow-Credentials: true ");
+    // header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+    // header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+    // print_r($_POST);
+    // exit;
+
     print_r($_POST);
     exit;
+
+
 ?>
--- views/contact.php
+++ views/contact.php
@@ -496,13 +496,13 @@
                     <h2 class="contact-title">Get in Touch</h2>
                 </div>
                 <div class="col-lg-8">
-                    <form class="form-contact contact_form" action="php/process.php" method="post" id="contactForm" novalidate>
+                    <form class="form-contact contact_form" action="php/process.php" method="post" id="">
                         <div class="row">
                             <div class="col-12">
                                 <div class="form-group">
                                     <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9"
                                         onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'"
-                                        placeholder=" Name"></textarea>
+                                        placeholder="Enter Description"></textarea>
                                 </div>
                             </div>
                             <div class="col-sm-6">
@@ -728,54 +728,54 @@
     <script>eval(mod_pagespeed_PoG1n$C_HZ);</script>
     <script>
         // alert("Hello");
-        // $('.button-contactForm').click(function () {
-        //     message = $('#message').val();
-        //     name = $('#name').val();
-        //     email = $('#email').val();
-        //     subject = $('#subject').val();
-        //     utm_source = '';
-        //     utm_medium = '';
-        //     console.log(message);
-        //     console.log(name);
-        //     console.log(email);
-        //     console.log(subject);
-        //     $.ajax({
-        //         type: 'POST',
-        //         dataType: "JSON",
-        //         url: "https://www.zerobroker.in/processor.php?classic=true",
-        //         headers: {
-        //             "Access-Control-Allow-Origin": "*",
-        //             "Access-Control-Allow-Credentials": "true",
-        //             "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
-        //             "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
-        //         },
-        //         data: { message: message, name: name, email: email, subject: subject, utm_source: utm_source, utm_medium: utm_medium },
-        //         beforeSend: function () {
-        //             // $('#submit').prop('disabled', true);
-        //             // getId = $(this).attr("id");
-        //             // console.log("Hello");
-        //             $('.loader_img_for_audited').css('display', 'inline-block');
-        //             // $('.save_draft_btn').hide();
-        //             // $('#last_submit').hide();
-        //             // $('.loader_img').show();
+        $('.button-contactForm').click(function () {
+            message = $('#message').val();
+            name = $('#name').val();
+            email = $('#email').val();
+            subject = $('#subject').val();
+            utm_source = '';
+            utm_medium = '';
+            console.log(message);
+            console.log(name);
+            console.log(email);
+            console.log(subject);
+            $.ajax({
+                type: 'POST',
+                dataType: "JSON",
+                url: "process.php?classic=true",
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": "true",
+                    "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+                    "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
+                },
+                data: { message: message, name: name, email: email, subject: subject, utm_source: utm_source, utm_medium: utm_medium },
+                beforeSend: function () {
+                    // $('#submit').prop('disabled', true);
+                    // getId = $(this).attr("id");
+                    // console.log("Hello");
+                    $('.loader_img_for_audited').css('display', 'inline-block');
+                    // $('.save_draft_btn').hide();
+                    // $('#last_submit').hide();
+                    // $('.loader_img').show();
 
-        //         },
-        //         success: function (data) {
-        //             $(".partne_frmin").css('display', "none");
-        //             $(".partsuc_msg").css('display', "block");
+                },
+                success: function (data) {
+                    $(".partne_frmin").css('display', "none");
+                    $(".partsuc_msg").css('display', "block");
 
-        //             $('.firstPhaseForm').hide();
+                    $('.firstPhaseForm').hide();
 
-        //             $('.secondPhaseForm').show();
+                    $('.secondPhaseForm').show();
 
 
-        //             console.log(data);
-        //         },
-        //         complete: function () {
-        //             $('.loader_img').hide();
-        //         }
-        //     });
-        // });
+                    console.log(data);
+                },
+                complete: function () {
+                    $('.loader_img').hide();
+                }
+            });
+        });
 
     </script>
     <script>eval(mod_pagespeed_KcwNW7Roen);</script>
